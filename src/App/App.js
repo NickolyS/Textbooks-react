afeb36553bb37c53eb73a61fdd00005841d4680d
Van Linden <virturooms@gmail.com>
2021-09-01 20:53:09 +0400

    uuuu

--- src/App/App.js
+++ src/App/App.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import $ from 'jquery';
 import './App.css';
-// Textbooks1
+// Textbooks2
 
 const App = props => {
     const [data, setData] = useState([]);
